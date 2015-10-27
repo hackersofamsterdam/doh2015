782245c396aeb5b053898be3fb9841e0241f2127
zenry <henry@postb.us>
2015-10-27 15:08:17 +0100

    dummy api endpoints

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,12 @@
+# ApiController
+# FAKE controller to show a FAKE JSON string of product names and ID's
+# IF there is time we'll implement the real API connections
+class ApiController < AdminController
+  def ah
+    # return prefetched FAKE JSON string
+  end
+
+  def bol
+    # return prefetched FAKE JSON string
+  end
+end
