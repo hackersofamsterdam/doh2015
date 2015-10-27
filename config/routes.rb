70226d34ed017fc03a8636ae9d036c6a3ea77cb2
zenry <henry@postb.us>
2015-10-27 15:07:37 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,38 @@
 Rails.application.routes.draw do
-  scope '/admin' do
+  scope 'admin' do
     devise_for :users
     resources :channels do
-      resources :products
+      resources :products, shallow: true
     end
+
+    devise_scope :user do
+      authenticated :user do
+        root to: 'dashboard#index',
+             as: :authenticated_root
+      end
+
+      unauthenticated do
+        root to: 'devise/sessions#new',
+             as: :unauthenticated_root
+      end
+    end
+
+    match '/api/bol', to: 'api#bol',
+                      as: :api_bol,
+                      via: [:get, :post]
+
+    match '/api/ah', to: 'api#ah',
+                     as: :api_ah,
+                     via: [:get, :post]
+
+    match '/', to: 'dashboard#index',
+               as: :dashboard,
+               via: :get
   end
+
+  matc '/redirect/:token', to: 'site#redirect',
+                           as: :product_redirect_link,
+                           via: :get
 
   root 'site#index'
 end
